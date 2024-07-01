6a99f8072459ee93512f1694e64502bf5da3417a
Aaron Freeze <afreeze93@gmail.com>
2024-06-30 22:44:18 -0500

    Add project files.

---
+++ WeatherApp.Server/Models/Customer.cs
@@ -0,0 +1,9 @@
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Contact { get; set; }
+    public string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
